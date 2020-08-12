45e1c9a710e7f3cb18a773bf899edd271da49829
Brandon Walderman <brwalder@microsoft.com>
2020-08-12 12:50:22 -0700

    Remove install step.

--- Run.ps1
+++ Run.ps1
@@ -1,4 +1,1 @@
-Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?linkid=2084706"&"Channel=Canary"&"language=en' -OutFile MicrosoftEdgeSetup.exe;
-Start-Process .\MicrosoftEdgeSetup.exe -ArgumentList '/silent /install' -Wait;
-
 py .\webdriver.py
